5dccd0938701c2b5cc15d424f8eb8cc7b1e1c78b
Bruno Lins <linslimabruno@gmail.com>
2023-11-19 09:32:24 -0300

    fazendo inputs, criando pastas para os componentes e incluindo os arquivos primordiais, configurando padding de acordo com a estrutura.

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,3 +1,20 @@
-const BarraLateral = () => <aside>Barra Lateral</aside>
+import FiltroCard from '../../components/FiltroCard'
+import * as S from './styles'
+
+const BarraLateral = () => (
+  <S.Aside>
+    <div>
+      <S.Campo type="text" placeholder="Buscar" />
+      <S.Filtros>
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+      </S.Filtros>
+    </div>
+  </S.Aside>
+)
 
 export default BarraLateral
