12629b6d7b94cd31dfd120c121caf5cc0e8bd3e4
Bruno Lins <linslimabruno@gmail.com>
2023-11-24 15:21:02 -0300

    criando pagina de cadastro de uma nova tarefa, estilizando botao da pagina, importando component LINK

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import BoataoAdicionar from '../../components/BotaoAdicionar'
 import BarraLateral from '../../containers/BarraLateral'
 import ListaDetarefas from '../../containers/ListaDeTarefas'
 
@@ -5,6 +6,7 @@ const Home = () => (
   <>
     <BarraLateral />
     <ListaDetarefas />
+    <BoataoAdicionar />
   </>
 )
 
